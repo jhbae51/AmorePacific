42f3b2acc93e360bff98d5fb37f4cd4908f80bc7
배정현 <jhbae@the-51.com>
2023-09-04 23:33:16 +0900

    B main

--- AmorePacific_B/assets/js/script.js
+++ AmorePacific_B/assets/js/script.js
@@ -5,7 +5,7 @@ history.scrollRestoration = "auto"
 /* ---- swiper ---- */  
 const mainSwiper = new Swiper('.main-swiper', {
   autoplay: {
-    delay: 4000,
+    delay: 3000,
     disableOnInteraction: false,
   },
   effect: "fade",
@@ -28,6 +28,10 @@ var mainSwiper2 = new Swiper(".main-swiper2", {
 
 var mainSwiper3menu = ['Architecture', 'sustainability', 'Customer Safety', 'Beauty Research & Innovation', 'Space' ]
 const mainSwiper3 = new Swiper('.main-swiper3', {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   effect: "fade",
   loop:true,
   loopAdditionalSlides: 1,	
@@ -60,58 +64,32 @@ var mainSwiper4 = new Swiper(".main-swiper4", {
 /* ---- scroll ---- */  
 function gnbScrollEvent() {
   let documentHeight = $(document).scrollTop();
-  let section1Top = jQuery('.main-wrap #section1').offset().top;
   let section4Top = jQuery('.main-wrap #section4').offset().top;
-  let $gnb = $('#gnb .gnb__wrap li a');
-  let $header = $('header');
-  let $language = $('#gnb .gnb__wrap .gnb__language');
-  let $logo = $('#gnb .gnb__wrap .gnb__1d-wrap');
-  let $right = $('#gnb .gnb__wrap .right');
-  let $left = $('#gnb .gnb__wrap .left');
+  let $section3 = $('.main-wrap #section3');
+  let $section4 = $('.main-wrap #section4');
+  let $section5 = $('.main-wrap #section5');
+  
+  $section3.addClass('color3');
+  $section3.removeClass('color4');
+  $section4.addClass('color3');
+  $section4.removeClass('color4');
+  $section5.addClass('color3');
+  $section5.removeClass('color4');
 
-
-  // console.log(documentHeight, section1Top);
-  if (documentHeight >= section1Top+300) {
-    $right.removeClass('right2');
-    $left.removeClass('left2');
-    $right.addClass('right1');
-    $left.addClass('left1');
-    $header.hover(
-      function() {
-        $right.removeClass('right1');
-        $left.removeClass('left1');    
-        $right.addClass('right2');
-        $left.addClass('left2');
-      }, function() {
-        $right.removeClass('right2');
-        $left.removeClass('left2');
-        $right.addClass('right1');
-        $left.addClass('left1');
-      }
-    );
+  if (documentHeight >= section4Top-500) {
+    $section3.addClass('color4');
+    $section3.removeClass('color3');
+    $section4.addClass('color4');
+    $section4.removeClass('color3');
+    $section5.addClass('color4');
+    $section5.removeClass('color3');
   } else {
-    $right.removeClass('right1');
-    $left.removeClass('left1');
-    $right.addClass('right2');
-    $left.addClass('left2');
-    $logo.hover(
-      function() {
-        $right.removeClass('right1');
-        $left.removeClass('left1');
-      }
-    );
-  }
-
-  if (documentHeight >= section4Top-100) {
-    $gnb.addClass('color1');
-    $gnb.removeClass('color2');
-    $language.addClass('color1');
-    $language.removeClass('color2');
-  } else {
-    $gnb.addClass('color2');
-    $gnb.removeClass('color1');
-    $language.addClass('color2');
-    $language.removeClass('color1');
+    $section3.addClass('color3');
+    $section3.removeClass('color4');
+    $section4.addClass('color3');
+    $section4.removeClass('color4');
+    $section5.addClass('color3');
+    $section5.removeClass('color4');
   }
 } 
 
@@ -164,22 +142,4 @@ function se3HoverEvent() {
 }
 $(document).ready(function () {
   se3HoverEvent();
-});
-
-
-const scrollWave = document.querySelector('.scroll-wave');
-const windowHeight = window.innerHeight;
-let prevScrollPosition = 0;
-
-window.addEventListener('scroll', function () {
-  const scrollPosition = window.scrollY;
-
-  // 스크롤 방향 확인
-  if (scrollPosition > prevScrollPosition) {
-    // 스크롤이 내려가는 방향일 때만 스케일 변경
-    const waveScale = 1 - (scrollPosition / windowHeight);
-    scrollWave.style.transform = `scaleY(${waveScale})`;
-  }
-
-  prevScrollPosition = scrollPosition;
 });
